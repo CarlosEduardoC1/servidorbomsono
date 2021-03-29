8c907d8e3564fef8d19e11a2ca4d84c3e269f45d
CarlosEduardo-RN <cnobre.trabalho@gmail.com>
2021-03-29 07:56:55 -0300

    try cors

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,11 @@ var controller = require('../controllers/jwt-auth/token');
 var auth = require("../middlewares/auth-jwt")();
 
 /* JWT AUTH */
+router.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 router.get('/', auth.authenticate(), controller.get);
 router.post('/', controller.post);
 
